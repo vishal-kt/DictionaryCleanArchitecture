a804cb63068006c1611a32fe25f4294f76618207
VISHAL <vishalstar895@gmail.com>
2023-07-16 23:18:50 +0530

    this is first commit of july revision

---
+++ app/src/main/java/com/techdeity/pineenergy/BasicsRevision.kt
@@ -0,0 +1,14 @@
+package com.techdeity.pineenergy
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Basics(){
+
+  Text("Vishal Kumar Text Example",
+            fontSize = 24.sp
+      )
+
+}
--- app/src/main/java/com/techdeity/pineenergy/MainActivity.kt
+++ app/src/main/java/com/techdeity/pineenergy/MainActivity.kt
@@ -3,12 +3,7 @@ package com.techdeity.pineenergy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.techdeity.pineenergy.ui.theme.PineEnergyTheme
 
@@ -18,29 +13,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             PineEnergyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Basics()
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    PineEnergyTheme {
-        Greeting("Android")
-    }
+
+    Basics()
 }
